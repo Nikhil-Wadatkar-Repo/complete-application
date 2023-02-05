a83b6cf28c4b0e3c5717371d0946ec612c2180c2
nikhil.wadatkar4693@gmail.com <nikhil.wadatkar4693@gmail.com>
2023-02-06 02:36:48 +0530

    identification-service project structre added

---
+++ identification-service/src/test/java/com/bs/identificationservice/IdentificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.identificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
